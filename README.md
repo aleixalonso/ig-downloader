d673940522d4f5daaa306c8d2d5627e1dc982e58
Aleix Alonso <aleixalonso@MacBook-Pro.local>
2020-06-10 16:20:34 +0200

    added support to check friendship, requires login

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ pip3 install instaloader
 python3 instagram-downloader.py option username
 ```
 * Option 1: download all instagram photos (and videos) for a given user
-    * Example: ```python3 instagram-downloader.py 1 yourinstagramusername```
+    * Example: ```python3 instagram-downloader.py 1 instagramusername```
 * Option 2: download IGTVs for a given user
-    * Example: ```python3 instagram-downloader.py 2 yourinstagramusername```
+    * Example: ```python3 instagram-downloader.py 2 instagramusername```
+* Option 3: check friendship between your instagram and another instagram, you must log in, it will show if you follow that account, and if they follow you 
+    * Example: ```python3 instagram-downloader.py 4 yourinstagramusername instagramusername```
--- instagram-downloader.py
+++ instagram-downloader.py
@@ -3,7 +3,6 @@ from instaloader import Instaloader, Profile
 
 def downloadAllPosts(L, username ):
     PROFILE = username
-   # L = Instaloader()
     profile = Profile.from_username(L.context, PROFILE)
     posts = profile.get_posts()
     total = len(list(profile.get_posts()))
@@ -26,17 +25,30 @@ def downloadProfilePic(L, username ):
     L.download_profilepic(profile)
     print("Profile picture downloaded")
 
-'''
-#you have to be logged
-def checkFriendship( username1, username2 ):
-    L = Instaloader()
+def checkFriendship(L, username1, username2 ):
+    L.interactive_login(username1)
     PROFILE1 = username1
     profile1 = Profile.from_username(L.context, PROFILE1)
     PROFILE2 = username2
     profile2 = Profile.from_username(L.context, PROFILE2)
     followers_list = profile1.get_followers()
-    print(len(list(followers_list)))
-'''
+    following_list = profile1.get_followees()
+    followers_usernames = []
+    following_usernames = []
+    for follower in followers_list:
+        followers_usernames.append(follower.username)
+    for follower in following_list:
+        following_usernames.append(follower.username)
+    
+    if(username2 in following_usernames):
+        print( username1 + " follows " + username2 )
+    else:
+        print( username1 + " does not follow " + username2 )
+    
+    if(username2 in followers_usernames):
+        print( username2 + " follows " + username1 )
+    else:
+        print( username2 + " does not follow " + username1 )
 
 def main(argv):
     mode = int(sys.argv[1])
@@ -61,7 +73,7 @@ def main(argv):
         if (profile2.is_private):
             print("user " + username2 + " is private")
 
-    if mode > 3:
+    if mode > 4:
         print("nothing here")
         exit(0)
     if mode == 1:
@@ -71,8 +83,7 @@ def main(argv):
     elif mode == 3:
         downloadProfilePic(L, username1)
     elif mode == 4:
-        #checkFriendship(username1, username2)
-        print("coming")
+        checkFriendship(L, username1, username2)
     else:
         print("nothing here")
 
