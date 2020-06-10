fe966c56b69443a4deb4d67146909a51c3ca0b67
Aleix Alonso <aleixalonso@MacBook-Pro.local>
2020-06-10 10:43:00 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # instagram-downloader
 Python Instagram downloader based on Instaloader library
+
+## Requirements
+Install Instaloader library
+```elm
+pip3 install instaloader
+```
+
+## Running instagram-downloader
+```elm
+python3 instagram-downloader.py option username
+```
---
+++ instagram-downloader.py
@@ -0,0 +1,31 @@
+import sys
+from instaloader import Instaloader, Profile
+
+def downloadAllPosts( username ):
+    PROFILE = username
+    L = Instaloader()
+    profile = Profile.from_username(L.context, PROFILE)
+    if (profile.is_private):
+        print("user is private")
+        exit(0)
+    posts = profile.get_posts()
+    for post in posts:
+        L.download_post(post, PROFILE)
+
+
+def main(argv):
+    mode = int(sys.argv[1])
+    username = sys.argv[2]
+    if mode > 1:
+        print("nothing here")
+        exit(0)
+    #idea: first check if user is private, only if all func share
+    if mode == 1:
+        downloadAllPosts(username)
+    elif mode == 2:
+        print("not implemented")
+    else:
+        print("nothing here")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
